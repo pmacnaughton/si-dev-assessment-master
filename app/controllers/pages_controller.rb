13be752142ebbf0bdc088f11075c4a1e87c1edea
Piers MacNaughton <piers.macnaughton@gmail.com>
2017-05-17 15:24:10 +0000

    Finishing Task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  #def sms
-  #  raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
-  #end
+  def sms
+  end
 end
